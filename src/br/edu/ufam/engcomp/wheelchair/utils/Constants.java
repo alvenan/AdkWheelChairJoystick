60739a2657f6e962902c076102e6eb9d59d3cd85
alison.venancio <alison.venancio@gmail.com>
2014-12-09 10:47:44 -0400

    Adicionada função de calculo de byte a serem enviados

--- src/br/edu/ufam/engcomp/wheelchair/utils/Constants.java
+++ src/br/edu/ufam/engcomp/wheelchair/utils/Constants.java
@@ -1,34 +1,34 @@
 package br.edu.ufam.engcomp.wheelchair.utils;
 
 public class Constants {
-	public static final int STICK_NONE = 0;
-	public static final int STICK_UP = 1;
-	public static final int STICK_UPRIGHT = 2;
-	public static final int STICK_RIGHT = 3;
-	public static final int STICK_DOWNRIGHT = 4;
-	public static final int STICK_DOWN = 5;
-	public static final int STICK_DOWNLEFT = 6;
-	public static final int STICK_LEFT = 7;
-	public static final int STICK_UPLEFT = 8;
+    public static final int STICK_NONE = 0;
+    public static final int STICK_UP = 1;
+    public static final int STICK_UPRIGHT = 2;
+    public static final int STICK_RIGHT = 3;
+    public static final int STICK_DOWNRIGHT = 4;
+    public static final int STICK_DOWN = 5;
+    public static final int STICK_DOWNLEFT = 6;
+    public static final int STICK_LEFT = 7;
+    public static final int STICK_UPLEFT = 8;
 
-	public static final int LAYOUT_BORDER = 200;
-	public static final int STICK_ALPHA = 100;
-	public static final int LAYOUT_ALPHA = 150;
-	public static final int OFFSET = 90;
-	public static final int MIN_DISTANCE = 20;
-	public static final int STICK_WIDTH = 150;
-	public static final int STICK_HEIGHT = 150;
-	
-	public static final int A_HUNDRED_PERCENT = 100;
+    public static final int LAYOUT_BORDER = 200;
+    public static final int STICK_ALPHA = 100;
+    public static final int LAYOUT_ALPHA = 150;
+    public static final int OFFSET = 90;
+    public static final int MIN_DISTANCE = 20;
+    public static final int STICK_WIDTH = 150;
+    public static final int STICK_HEIGHT = 150;
+
+    public static final int A_HUNDRED_PERCENT = 100;
     public static final int CENTRAL_VALUE = 152;
-    public static final int UP_VALUE = 98/100;
-    public static final int DOWN_VALUE = 9/100;
-    public static final int RIGHT_VALUE = 9/100;
-    public static final int LEFT_VALUE = 98/100;
+    public static final int UP_VALUE = 98 / 100;
+    public static final int DOWN_VALUE = 9 / 100;
+    public static final int RIGHT_VALUE = 9 / 100;
+    public static final int LEFT_VALUE = 98 / 100;
     public static final byte PATTERN_COMMAND_VALUE = 0x20;
     public static final int PATTERN_DAC_VALUE = 152;
-	
-	public static final int RQS_USB_PERMISSION = 0;
-	public static final String ACTION_USB_PERMISSION = "arduino-er.usb_permission";
+
+    public static final int RQS_USB_PERMISSION = 0;
+    public static final String ACTION_USB_PERMISSION = "arduino-er.usb_permission";
 
 }
